911469c984e83787f84c75a16cedcf22eb3826fc
harrisonruffo-cpu <harrisonruffo@gmail.com>
2026-08-31 16:48:43 -0300

    Update build.gradle.kts
    Configura Appwrite no projeto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,7 @@ googleServices { missingGoogleServicesStrategy = MissingGoogleServicesStrategy.W
 // Some unused dependencies are commented out below instead of being removed.
 // This makes it easy to add them back in the future if needed.
 dependencies {
+implementation("io.appwrite:sdk-for-android:8.1.0")
   implementation(platform(libs.androidx.compose.bom))
   implementation(platform(libs.firebase.bom))
   // implementation(libs.accompanist.permissions)
