ac4c4ac27b4388bbb5abe1bc2866e1b3c1a92eac
harrisonruffo-cpu <harrisonruffo@gmail.com>
2026-08-31 16:59:04 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,7 @@ googleServices { missingGoogleServicesStrategy = MissingGoogleServicesStrategy.W
 // This makes it easy to add them back in the future if needed.
 dependencies {
 implementation("io.appwrite:sdk-for-android:8.1.0")
+implementation("io.appwrite:sdk-for-android:8.1.0")
   implementation(platform(libs.androidx.compose.bom))
   implementation(platform(libs.firebase.bom))
   // implementation(libs.accompanist.permissions)
